843291fa33651f4f6a3e6382dec45374fd97955f
Matthew Canty <mcanty@sportingindex.com>
2014-07-14 21:41:55 +0000

    Take out logging in router

--- router.js
+++ router.js
@@ -3,9 +3,6 @@ var static = require('node-static');
 var fileServer = new static.Server(__dirname);
 
 function route(handle, pathname, response, request){
-  
-  console.log('routing /' + pathname);
-  
   if(typeof handle[pathname] === 'function') {
     handle[pathname](response, request);
   } else {
