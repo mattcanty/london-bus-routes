666bd401c0de9c4b1bfa808f4cc366a762a8ef26
Matthew Canty <mcanty@sportingindex.com>
2014-07-31 17:24:21 +0000

    Put in artificial wait before saying 'reticulating splines...'

--- js/busroutes.js
+++ js/busroutes.js
@@ -1,14 +1,21 @@
-function BusRoutesController($scope, busRoutesService){
+function BusRoutesController($scope, $timeout, busRoutesService){
     $scope.info = 'Where does this bus go?!';
+    
+    var splinesTimeout;
   
     $scope.findRoute = function(){
       updateInfo('Looking for ' + $scope.route + '...');
       
+      $timeout.cancel(splinesTimeout);
+      splinesTimeout = $timeout(function(){
+        updateInfo('Reticulating splines...');
+      }, 1000);
+      
       busRoutesService.search($scope.route).then(function(data){
-        updateInfo('Reticulating splines');
+        
         console.log(data);
         placeMarkers(data);
-        
+        $timeout.cancel(splinesTimeout);
         updateInfo('Here is ' + data._route + '\'s route!');
         
         resizeMap();
